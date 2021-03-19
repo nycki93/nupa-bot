3ddec6d39a585663603fedc319ae059dd605c864
Nicholas Lamicela <nupanick@gmail.com>
2021-03-19 11:48:55 -0700

    Bot can login and talk now!

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const Discord = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-console.log('Hello, ' + process.env.NAME + '!');
+const PREFIX = 'please ';
+
+const client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Hello, ' + process.env.NAME + '!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+	if (message.content === PREFIX + 'ping') {
+		message.channel.send('Pong.');
+	}
+});
+
+client.login(process.env.TOKEN);
