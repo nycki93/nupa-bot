87f1d4d6d8fa05b046bcb5561351825893b51eaf
Nicholas Lamicela <nupanick@gmail.com>
2021-03-21 21:38:54 -0700

    Defined wrapper types for clients and commands.

--- src/main.ts
+++ src/main.ts
@@ -2,41 +2,68 @@ import * as Discord from 'discord.js';
 import * as dotenv from 'dotenv';
 
 import ticTacToe from './ticTacToe.js';
+import { Client, Command, Message } from './types.js';
 
 dotenv.config();
 const PREFIX = '!';
-const client = new Discord.Client();
 
-const mono = (t: string) => '```' + t + '```';
+const dc = new Discord.Client();
+const client: Client = {
+    on: (messageHandler) => dc.on('message', discordMessage => (
+        messageHandler({
+            userId: discordMessage.member.id,
+            channelId: discordMessage.channel.id,
+            content: discordMessage.content,
+        })
+    )),
+    send: (message) => (dc.channels
+        .fetch(message.channelId)
+        .then(c => (c as Discord.TextChannel)
+            .send('```' + message.content + '```'))
+    ),
+}
+dc.once('ready', () => console.log('Bot started.'));
+dc.login(process.env.TOKEN);
+
+const commands: {[key: string]: Command} = {
+    ping: (state: any, message: Message) => ({
+        state,
+        replies: [{
+            userId: message.userId,
+            channelId: message.channelId,
+            content: 'pong!',
+        }],
+    }),
+    tictactoe: (state: any, message: Message) => {
+        const { newState, replies } = ticTacToe({
+            state, 
+            user: message.userId, 
+            channel: message.channelId, 
+            args: message.content.split(/\s+/),
+        });
+        return { 
+            state: newState, 
+            replies: Array.from(replies).map(r => ({
+                userId: r.user,
+                channelId: r.channel,
+                content: r.message,
+            })),
+        };
+    },
+}
 
 const stateCache = {};
 
-client.once('ready', () => {
-	console.log('Bot started.');
+client.on(message => {
+    const m = message.content.match('^' + PREFIX + '((\\w+).*)$');
+    if (!m || !m[2]) return;
+    const content = m[1];
+    const keyword = m[2];
+    
+    if (!(keyword in commands)) return;
+    const command = commands[keyword];
+    const oldState = stateCache[keyword] || {};
+    const { state, replies } = command(oldState, {...message, content});
+    stateCache[keyword] = state;
+    replies.forEach(client.send);
 });
-
-client.on('message', message => {
-    const m = message.content.match('^' + PREFIX + '(.+)( .+)?$');
-    if (!m || !m[1]) return;
-    console.log(m[0]);
-    const args = m[1].split(/\s+/);
-    const command = args[0];
-
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'tictactoe') {
-        const key = message.channel.id + ':tictactoe';
-        const { newState, replies } = ticTacToe({
-            state: stateCache[key] || {},
-            user: message.member.id,
-            channel: message.channel.id,
-            args,
-        })
-        stateCache[key] = newState;
-        for (const reply of replies) {
-            message.channel.send(mono(reply.message));
-        }
-    }
-}); 
-
-client.login(process.env.TOKEN);
--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,20 @@ export type ChatCommand = (o: {
         message: string,
     }>;
 };
+
+export interface Message {
+    userId: string,
+    channelId: string,
+    content: string,
+}
+
+export interface Client {
+    on: (messageHandler: (message: Message) => void) => void;
+    send: (message: Message) => void;
+}
+
+export type Command = (
+    state: any, message: Message,
+) => {
+    state: any, replies: Message[],
+}
