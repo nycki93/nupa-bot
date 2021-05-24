7f657d8ac2604d9d39fcb5adcfe241a705bf75d9
Nicholas Lamicela <nupanick@gmail.com>
2021-05-23 18:05:20 -0700

    Load apps from a subfolder.

--- source/bot.ts
+++ source/bot.ts
@@ -17,7 +17,21 @@ export function* init(): Generator<Effect, never, Report> {
                 continue;
             }
             if (args[0] === 'play') {
-                app = guessingGame();
+                if (!args[1]) {
+                    yield { type: 'write', text: 'Options: play <game>'};
+                    report = yield { type: 'read' };
+                    continue;
+                }
+                report = yield { type: 'load app', app: args[1] };
+                if (report.type !== 'load app') {
+                    throw TypeError(`Expected load app, but got ${report.type}`);
+                }
+                if (!report.app) {
+                    yield { type: 'write', text: 'Unable to load that app.'};
+                    report = yield { type: 'read' };
+                    continue;
+                }
+                app = report.app;
                 report = yield { type: 'write', text: 'guessing game started.' };
                 continue;
             }
@@ -42,35 +56,5 @@ export function* init(): Generator<Effect, never, Report> {
             throw TypeError(`Expected message, but got ${report?.type}.`);
         }
         report = yield { type: 'read' };
-    }
-}
-
-function* guessingGame(): Generator<Effect, null, Report> {
-    let report = yield { type: 'roll', max: 100 };
-    if (report.type !== 'roll') throw Error('Expected roll.');
-    const secret = report.value;
-    yield { type: 'write', text: "I'm thinking of a number between 1 and 100." };
-    yield { type: 'write', text: 'Options: guess <number>' };
-    while (true) {
-        const report = yield { type: 'read' };
-        if (report.type !== 'message') {
-            throw TypeError(`Expected message, but got ${report.type}.`);
-        }
-        const args = report.text.split(/\s+/);
-        if (args[0] != 'guess') {
-            yield { type: 'write', text: 'Options: guess <number>' };
-            continue;
-        }
-        const guess = parseInt(args[1]);
-        if (guess < secret) {
-            yield { type: 'write', text: 'Too low!' };
-            continue;
-        }
-        if (guess > secret) {
-            yield { type: 'write', text: 'Too high!' };
-            continue;
-        }
-        yield { type: 'write', text: "That's exactly right!" };
-        return;
     }
 }
