cb8debc014ba6acf1206edff28e759e4fb1ff43d
Nick Lamicela <nupanick@gmail.com>
2023-05-06 15:22:48 -0700

    Break game logic out into module

---
+++ source/bot.ts
@@ -0,0 +1,120 @@
+interface App { 
+    init(): BotResponse,
+    commands(): string[],
+    help(args: string[]): BotResponse,
+    handle(args: string[]): BotResponse, 
+}
+
+export interface BotResponse {
+    message?: string,
+    error?: string,
+    quit?: boolean,
+}
+
+export class Bot implements App {
+    game?: App = null;
+
+    init() {
+        return { message: 'Bot loaded!' };
+    }
+
+    commands() {
+        if (this.game) {
+            return ['help', 'ping', ...this.game.commands()];
+        } else {
+            return ['help', 'ping', 'play'];
+        }
+    }
+
+    help([a0, ...args]: string[]) {
+        if (a0 === 'ping') {
+            return { message: 'Check if the bot is running.' };
+        } else if (a0 === 'help') {
+            return { message: 'Usage: help <command>' };
+        } else if (this.game && this.game.commands().includes(a0)) {
+            return this.game.help([a0, ...args]);
+        } else if (!this.game && a0 === 'play') {
+            return { message: 'Start a game. Games available: guess' };
+        } else {
+            return { message: `Commands: ${this.commands().join(', ')}` };
+        }
+    }
+
+    handle([a0, ...args]: string[]) {
+        if (a0 === 'ping') {
+            return { message: 'Pong!' };
+        } else if (a0 === 'help') {
+            return this.help(args);
+        } else if (this.game && this.game.commands().includes(a0)) {
+            const { quit, ...r } = this.game.handle([a0, ...args]);
+            if (quit) {
+                this.game = null;
+            }
+            return r;
+        } else if (!this.game && a0 === 'play') {
+            return this.play(args);
+        } else {
+            return null;
+        }
+    }
+
+    play([a0]: string[]) {
+        if (a0 === 'guess') {
+            this.game = new GuessingGame();
+            return this.game.init();
+        } else {
+            return { message: 'Games available: guess' };
+        }
+    }
+}
+
+class GuessingGame implements App {
+    solution: number;
+
+    init() {
+        this.solution = 69;
+        return { message: "Guessing game started! Guess my number with 'guess'." };
+    }
+
+    commands() {
+        return [ 'guess', 'give-up' ];
+    }
+
+    help([a0]: string[]) {
+        if (a0 === 'guess') {
+            return { message: 'Usage: guess <number>' };
+        } else if (a0 === 'give-up') {
+            return { message: 'Reveal the number and end the game.' };
+        } else {
+            return { error: `No command ${a0} in GuessingGame` };
+        }
+    }
+
+    handle([a0, ...args]: string[]) {
+        if (a0 === 'give-up') {
+            return { 
+                message: `Game over! The answer was ${this.solution}.`,
+                quit: true,
+            }
+        } else if (a0 === 'guess') {
+            return this.guess(args) || this.help(['guess']);
+        }
+    }
+
+    guess([a0, ...args]: string[]) {
+        if (args.length) return null;
+        const n = Number(a0);
+        if ( Number.isNaN(n) ) return null;
+
+        if (n < this.solution) {
+            return { message: 'Too low!' };
+        } else if (n > this.solution) {
+            return { message: 'Too high!' };
+        } else {
+            return { 
+                message: `That's right! It was ${this.solution}.`,
+                quit: true,
+            }
+        }
+    }
+}
--- source/main.ts
+++ source/main.ts
@@ -2,6 +2,8 @@ import * as fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { Client, Events, GatewayIntentBits, TextChannel } from 'discord.js';
 
+import { Bot, BotResponse } from './bot.js';
+
 type Config = {
     prefix: string,
     token: string,
@@ -27,100 +29,48 @@ function readWriteConfig(path = 'config.json') {
 }
 
 function main() {
-    let state = {
-        game: {
-            playing: null,
-            message: null,
-        }
-    };
     const client = new Client({ intents: [ 
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
     ]});
     const config = readWriteConfig();
+    const bot = new Bot();
 
     client.once(Events.ClientReady, async c => {
         console.log(`Connected as ${c.user.tag}`);
-        const channel = await c.channels.fetch(config.channel);
-        if (channel.isTextBased) {
-            (channel as TextChannel).send('Bot connected!');
+        const _ch = await c.channels.fetch(config.channel);
+        if (!_ch.isTextBased) {
+            console.log("Can't connect bot to non-text channel!");
+            process.exit(0);
         }
+        const ch = _ch as TextChannel;
+        const r = bot.init();
+        ch.send(r.message);
     });
 
     client.on(Events.MessageCreate, m => {
         if (!m.content.startsWith('!')) return;
-        const args = m.content.slice('!'.length).split(/\s/);
-        console.log(args);
-        const command = args[0].toLowerCase();
-        if (command === 'help') {
-            m.channel.send('Commands: help, ping, game');
-        } else if (command === 'ping') {
-            m.channel.send('pong!');
-        } else if (command === 'game') {
-            state.game = game({ state: state.game, args });
-            if (state.game.message) {
-                m.channel.send(state.game.message);
+        const [a0, ...args] = m.content.slice('!'.length).split(/\s/);
+        if (bot.commands().includes(a0)) {
+            let r: BotResponse;
+            try {
+                r = bot.handle([a0, ...args]);
+            } catch (e) {
+                console.log(e);
+                m.channel.send(`[CRASH]: ${e}`);
+                process.exit(0);
+            }
+            if (r.error) {
+                m.channel.send(`[ERROR] ${r.error}`);
+            }
+            if (r.message) {
+                m.channel.send(r.message);
             }
         }
     });
 
     client.login(config.token);
-}
-
-function game(o: { state: any, args: string[]}) {
-    const { state, args } = o;
-    const subcommand = args.length > 1 && args[1];
-    if (state.playing === null && (!subcommand || subcommand === 'help')) {
-        return {
-            ...state,
-            message: `Usage: ${args[0]} start guess`,
-        }
-    }
-    if (state.playing === null && subcommand === 'start') {
-        const choice = args.length > 2 && args[2];
-        if (choice === 'guess') {
-            return {
-                ...state,
-                playing: 'guess',
-                solution: 69,
-                message: 'Guessing game started! Guess my number with !game guess <number>.',
-            }
-        }
-        return {
-            ...state,
-            message: `Available games: guess`,
-        }
-    }
-    if (state.playing === 'guess') {
-        const guess = args.length > 2 && Number(args[2]);
-        if (!guess) {
-            return {
-                ...state,
-                message: `Usage: ${args[0]} ${args[1]} <number>`,
-            }
-        }
-        if (guess < state.solution) {
-            return {
-                ...state,
-                message: 'Too low!',
-            }
-        }
-        if (guess > state.solution) {
-            return {
-                ...state,
-                message: 'Too high!',
-            }
-        }
-        return {
-            playing: null,
-            message: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry. I'm excited. Here's the graduate. We're very proud of you, son. A perfect report card, all B's. Very proud."
-        }
-    }
-    return {
-        ...state,
-        message: `Usage: ${args[0]} start guess`,
-    }
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
