33e0b507db6da1384685e8aaaffdd7282f7588fa
Nick Lamicela <nupanick@gmail.com>
2023-05-06 21:17:26 -0700

    Add win condition to tictactoe

--- source/tictactoe.ts
+++ source/tictactoe.ts
@@ -6,9 +6,7 @@ export class TictactoeGame implements App {
     init() {
         return { message: [
             'Tictactoe Started.',
-            '```',
             draw(this.board),
-            '```',
             "move with 'play <location> <x|o>'"
         ].join('\n')};
     }
@@ -27,42 +25,64 @@ export class TictactoeGame implements App {
         }
     }
 
-    handle([a0, a1, a2, ...args]) {
-        if (a0 === 'resign' && !a1) {
+    handle([a0, ...args]) {
+        if (a0 === 'resign' && !args.length) {
             return {
                 message: 'Game over.',
                 quit: true,
             }
         } else if (a0 === 'play') {
-            const location = Number(a1);
-            const symbol = a2;
-            if (
-                args.length
-                || ![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(location)
-                || !['x', 'o'].includes(symbol)
-            ) {
-                return this.help(['play']);
-            }
-            this.board[location-1] = symbol;
-            return {
-                message: [
-                    '```',
-                    draw(this.board),
-                    '```',
-                ].join('\n'),
-            }
+            return this.play(args) || this.help([a0]);
         } else {
             return { error: `Unknown command ${a0} in TictactoeGame.`};
+        }
+    }
+
+    play([a0, a1, ...args]: string[]) {
+        const location = Number(a0);
+        const symbol = a1;
+        if (
+            args.length
+            || ![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(location)
+            || !['x', 'o'].includes(symbol)
+        ) {
+            return null;
+        }
+        if (this.board[location-1]) {
+            return { message: 'That spot is already claimed!' };
+        }
+        this.board[location-1] = symbol;
+        const winner = getWinner(this.board);
+        if (winner) {
+            return { 
+                message: [
+                    draw(this.board),
+                    `${winner} is the winner!`,
+                ].join('\n'),
+                quit: true,
+            }
+        } else if (isFull(this.board)) {
+            return { 
+                message: [
+                    draw(this.board),
+                    'The game is a draw.',
+                ].join('\n'),
+                quit: true,
+            }
+        } else {
+            return { message: draw(this.board) };
         }
     }
 }
 
 const template = [
+    '```',
     ' 1 | 2 | 3 ',
     '---+---+---',
     ' 4 | 5 | 6 ',
     '---+---+---',
     ' 7 | 8 | 9 ',
+    '```',
 ].join('\n');
 function draw(board: string[]) {
     let result = template;
@@ -71,4 +91,22 @@ function draw(board: string[]) {
         result = result.replace((i+1).toString(), v);
     });
     return result;
+}
+
+const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+];
+function getWinner(board: string[]) {
+    const line = lines.find(([a, b, c]) => board[a] === board[b] && board[a] === board[c]);
+    if (line) {
+        return board[line[0]];
+    } else {
+        return null;
+    }
+}
+
+function isFull(board: string[]) {
+    return Boolean(board.find(a => !a));
 }
